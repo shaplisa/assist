fe77808151cfcac17103bf4bbedc95bed0b030f6
Shaplisa <shaplisa@mail.ru>
2026-02-14 16:26:07 +0300

    dsfsd

--- app/config.py
+++ app/config.py
@@ -8,6 +8,6 @@ WIFI = [
 SUDO_PASS = "lisa682907"
 
 # Настройка пинов (BCM нумерация)
-BUTTON_PIN = 5   # GPIO5 (физический пин 29)
-BUTTON_OFF_PIN = 6   # GPIO6 (физический пин 31)
+BUTTON_OFF_PIN = 5   # GPIO5 (физический пин 29)
+BUTTON_PIN = 6   # GPIO6 (физический пин 31)
 
